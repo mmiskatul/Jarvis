8cf8e51cf3602bb3aa06d991b5dd2e308d7edf7d
Md Miskatul Masabi <masabimiskat@gmail.com>
2026-01-12 21:43:43 +0600

    add print_loop function

--- Head/Ear.py
+++ Head/Ear.py
@@ -6,6 +6,12 @@ from colorama import Fore,Style,init
 
 init(autoreset=True)
 
+def print_loop():
+    while True:
+        print(Fore.LIGHTGREEN_EX + "I am Listening ..... ",end="",flush=True)
+        print(Style.RESET_ALL,end="",flush=True)
+        print("",end="",flush=True)
+
 
 
 
