a5f20e36f40737615a6cee93b539b487d978809e
Md Miskatul Masabi <masabimiskat@gmail.com>
2026-01-12 22:17:35 +0600

    add try and expect abd final

--- Head/Ear.py
+++ Head/Ear.py
@@ -27,6 +27,35 @@ def listen():
     rec.pause_threshold =0.2
     rec.non_speaking_duration =0.3
 
+    with sr.Microphone() as source:
+        rec.adjust_for_ambient_noise(source)
+        while True:
+            print(Fore.LIGHTGREEN_EX + "I am Listening ..... ", end="", flush=True)
+            try:
+                audio = rec.listen(source,timeout=None)
+                print("\r"+Fore.LIGHTYELLOW_EX + "Got it , Recognizing...",end="",flush=True)
+                recognized_txt =rec.recognize_google(audio).lower()
+                if recognized_txt:
+                        translated_txt =trans_bengali_to_english(recognized_txt)
+                        print("\r"+Fore.BLUE + "Mr Miskat : "+translated_txt )
+                else :
+                    return ""
+            except sr.UnknownValueError:
+                recognized_txt = ""
+            finally:
+                print("\r",end="",flush=True)
+            os.system("cls" if os.name == "nt" else "clear")
+            #threding part
+            listen_thread = threading.Thread(target=listen)
+            print_thread = threading.Thread(target=print_loop())
+            listen_thread.start()
+            print_thread.start()
+            listen_thread.join()
+            print_thread.join()
 
+
+
+
+listen()
 
 
