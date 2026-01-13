8c0b65d55439a8468b38cff7dd0c5d8511eec525
Md Miskatul Masabi <masabimiskat@gmail.com>
2026-01-13 20:29:44 +0600

    add file remove_file

--- Head/Mouth.py
+++ Head/Mouth.py
@@ -4,3 +4,17 @@ import edge_tts
 import pygame
 import time
 
+VOICE = "en-AU-WilliamNeural"
+
+
+def remove_file(filename):
+    for _ in range(3):
+        try:
+            if os.path.exists(filename):
+                os.remove(filename)
+            break
+        except Exception as e:
+            print(f"File remove error: {e}")
+            time.sleep(0.2)
+
+
